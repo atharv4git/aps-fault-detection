04c513581e9ec10dd15f98973d580140f2e180bd
atharv4git <atharv4study@gmail.com>
2023-01-07 15:01:08 +0530

    certain ipdates made

--- test.py
+++ test.py
@@ -1,17 +1,10 @@
 from sensor.exception import SensorException
 from sensor.logger import logging
 import sys,os
-
-def test_logger_and_exception():
-    try:
-        logging.info("start")
-        result = 3/0
-        print(result)
-    except Exception as e:
-        raise e
+from sensor.utils import get_collections_as_dataframe
 
 if __name__ == "__main__":
     try:
-        pass
+        get_collections_as_dataframe(database_name="aps", collection_name="sensor")
     except Exception as e:
         print(e)
